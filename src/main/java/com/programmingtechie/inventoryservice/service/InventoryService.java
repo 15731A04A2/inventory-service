1c2c8b884883baea9aa1b6fbc5a232c35136646d
jonna <jonnalagaddasai1998@gmail.com>
2024-09-25 16:51:52 +0530

    #added inventory service

---
+++ src/main/java/com/programmingtechie/inventoryservice/service/InventoryService.java
@@ -0,0 +1,25 @@
+package com.programmingtechie.inventoryservice.service;
+
+import com.programmingtechie.inventoryservice.dto.InventoryResponse;
+import com.programmingtechie.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        log.info("Checking Inventory");
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+}
