dc0c091f51747abee725cef295d1f7ad642606e7
jonna <jonnalagaddasai1998@gmail.com>
2024-09-26 17:08:06 +0530

    #implemented inventory service to accept list of skucodes

--- src/main/java/com/programmingtechie/inventoryservice/service/InventoryService.java
+++ src/main/java/com/programmingtechie/inventoryservice/service/InventoryService.java
@@ -3,7 +3,6 @@ package com.programmingtechie.inventoryservice.service;
 import com.programmingtechie.inventoryservice.dto.InventoryResponse;
 import com.programmingtechie.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,8 +17,15 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
         log.info("Checking Inventory");
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                        InventoryResponse.builder()
+                                .skuCode(inventory.getSkuCode())
+                                .isInStock(inventory.getQuantity() > 0)
+                                .build()
+                ).toList();
     }
 }
+
